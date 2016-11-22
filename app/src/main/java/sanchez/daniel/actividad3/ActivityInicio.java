c46d525cfd574aa78975bd56813880ac43daaf40
Daniel Sánchez Rodríguez <srdaniel83@gmail.com>
2016-11-22 10:50:19 +0100

    Root Commit: Se inicializan los Fragments para login, registro y creditos. Se crean el MainActivity y el ActivityInicio

---
+++ app/src/main/java/sanchez/daniel/actividad3/ActivityInicio.java
@@ -0,0 +1,7 @@
+package sanchez.daniel.actividad3;
+
+/**
+ * Created by daniel.rodriguez on 22/11/2016.
+ */
+public class ActivityInicio {
+}
---
+++ libreria/src/main/java/sanchez/daniel/libreria/QBAdmin.java
@@ -0,0 +1,7 @@
+package sanchez.daniel.libreria;
+
+/**
+ * Created by daniel.rodriguez on 22/11/2016.
+ */
+public class QBAdmin {
+}
